012f7a11593f4a2711dd7f122f21438e0ca40390
Ordell17 <ST10467476@vcconnect.edu.za>
2025-03-12 07:25:33 +0200

    PrjClass
    Updating Class and Student

--- PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ PrjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -4,16 +4,35 @@
 
 package com.mycompany.prjclasses;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lab_services_student
  */
 public class PrjClasses {
+    
+    private static Students s = new Students();
 
     public static void main(String[] args) {
-        newClass nc = new newClass();
-        nc.helloClass(); //have to use variable name nc to communicate with the class
-        System.out.println(nc.strSecondClassVar);//output variable from second class
         
+        String strName = JOptionPane.showInputDialog(null, "Please enter student name");
+        String strCourse = JOptionPane.showInputDialog(null, "Please enter student course");
+        double dblAvg = Double.parseDouble(JOptionPane.showInputDialog(null, "Please enter student average result"));
+        
+        s. setName(strName);
+        s. setCourse(strCourse);
+        s. setAverage(dblAvg);
+        print();
     }
+    
+    public static void print()
+    {
+        String strDisplay = "The updated student average results are:" + "\n" + 
+                             "STUDENT: " + s.getName() + "\n" + "COURSE: " + 
+                              s.getCourse() + s.getCourse() + "\n" + "UPDATED AVERAGE: "
+                              + s.getAverage() + "";
+        
+        JOptionPane.showMessageDialog(null, strDisplay);
+    }        
 }
